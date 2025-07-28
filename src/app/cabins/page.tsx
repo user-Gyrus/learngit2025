206a2a0efee3e371d046e2ff10f07b0a79886e8a
user-Gyrus <nandakumarblj@gmail.com>
2025-07-28 19:27:21 +0530

    Added the next level of changes

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Counter from "../_components/Counter";
 
-export default function Page() {
-  return <h1>Cabins Page</h1>;
+export default async function Page() {
+  // const response = await fetch("example.com/api/cabins");
+  // const data = await response.json();
+
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+      <Counter />
+    </div>
+  );
 }
